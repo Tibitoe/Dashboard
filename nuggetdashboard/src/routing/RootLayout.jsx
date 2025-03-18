b8783a8d49df011618e8cb3ce5205596f29f94e1
Tobias Engvall <tobias.engvall@chasacademy.se>
2025-03-18 15:54:07 +0100

    changed import for footer

--- nuggetdashboard/src/routing/RootLayout.jsx
+++ nuggetdashboard/src/routing/RootLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../components/footer/footer";
 import Header from "../components/Header/Header";
+import Footer from "../components/footer/Footer";
 
 function RootLayout() {
   return (
