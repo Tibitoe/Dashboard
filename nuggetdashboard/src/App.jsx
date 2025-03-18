bd1d6b29803a38362e895b5ad8eeb54f1f234c7b
Tobias Engvall <tobias.engvall@chasacademy.se>
2025-03-18 15:51:13 +0100

    Uninstalled MUI, Bootstrap, Json-server, removed related components, files and imports. Remaking alot to be able to use Tailwind for all styling.

--- nuggetdashboard/src/App.jsx
+++ nuggetdashboard/src/App.jsx
@@ -1,7 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import route from "./routing/Routing";
 import "./index.css";
-import "mdb-react-ui-kit/dist/css/mdb.min.css";
 
 function App() {
   return <RouterProvider router={route} />;
--- nuggetdashboard/src/pages/Homepage.jsx
+++ nuggetdashboard/src/pages/Homepage.jsx
@@ -1,7 +1,4 @@
 import HeroVideo from "../components/Herovideo/HeroVideo";
-import BasicPie from "../components/charts/PieChart";
-import SimpleLineChart from "../components/charts/LineChartDeluxe";
-import CircularText from "../components/RotatingText/CircularText";
 
 function HomePage() {
   return (
@@ -10,13 +7,6 @@ function HomePage() {
       <h2 className="text-2xl font-bold text-center text-black">
         Important things in life
       </h2>
-      <div className="flex justify-center gap-8">
-        <BasicPie />
-        <SimpleLineChart />
-      </div>
-      <section className="mt-5">
-        <CircularText text="Bitcoin" />
-      </section>
     </div>
   );
 }
