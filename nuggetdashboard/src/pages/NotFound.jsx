310ac6241a2e6bfa5cb3cac707c64ac2c4d66bd6
Tobias Engvall <tobias.engvall@chasacademy.se>
2025-03-21 20:14:25 +0100

    fixed styling/colors on NotFound page

--- nuggetdashboard/src/pages/NotFound.jsx
+++ nuggetdashboard/src/pages/NotFound.jsx
@@ -2,11 +2,14 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="text-center m-5">
-      <h2>Hoppsan, sidan hittades inte!</h2>
+    <div className="text-center m-5 text-gray-900 dark:text-gray-200">
+      <h2 className="text-2xl font-bold">Hoppsan, sidan hittades inte!</h2>
       <p>Sidan du försökte nå finns inte.</p>
       <p>
-        Gå tillbaka till <Link to="/">Hem</Link>
+        Gå tillbaka till{" "}
+        <Link to="/" className="text-purple-400">
+          Hem
+        </Link>
       </p>
     </div>
   );
