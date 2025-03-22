92992f451af179d514306551758c0aed26059202
Tobias Engvall <tobias.engvall@chasacademy.se>
2025-03-22 08:59:17 +0100

    updated NotFound page

--- nuggetdashboard/src/pages/NotFound.jsx
+++ nuggetdashboard/src/pages/NotFound.jsx
@@ -3,12 +3,16 @@ import { Link } from "react-router-dom";
 export default function NotFound() {
   return (
     <div className="text-center m-5 text-gray-900 dark:text-gray-200">
-      <h2 className="text-2xl font-bold">Hoppsan, sidan hittades inte!</h2>
-      <p>Sidan du försökte nå finns inte.</p>
-      <p>
-        Gå tillbaka till{" "}
-        <Link to="/" className="text-purple-400">
-          Hem
+      <h2 className="text-8xl font-bold">404</h2>
+      <p className="text-3xl"> Sorry, we couldn't find this page.</p>
+      <br />
+      <p className="text-xl">
+        Go back to{" "}
+        <Link
+          to="/"
+          className="text-purple-500 font-bold hover:text-purple-700 dark:hover:text-purple-400"
+        >
+          Home
         </Link>
       </p>
     </div>
